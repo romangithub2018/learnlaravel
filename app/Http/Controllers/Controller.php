0d0c18e6a677f5c932aae6ab77f6f0acc1d78479
r.kistrui <r.kistrui@software-dep.net>
2018-05-25 14:59:47 +0300

    New release

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,9 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-/* Simple comment    */
+/* Simple comment */
+
+/* release */
 
 class Controller extends BaseController
 {
