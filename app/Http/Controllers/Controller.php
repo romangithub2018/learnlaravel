cc1982cdec77f001e4bb9fb15d039bca2ec4eaef
r.kistrui <r.kistrui@software-dep.net>
2018-05-25 13:45:56 +0300

    New one

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-/* Simple comment */
+/* Simple comment    */
 
 class Controller extends BaseController
 {
